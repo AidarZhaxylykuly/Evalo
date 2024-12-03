20e28fea345efcf1233bf122b7526147a3309b5f
Abilmansur <microsoft.ashim@gmail.com>
2024-12-03 23:26:50 +0500

    folder fix and unit tests for users app

---
+++ users/unit_tests/test_models.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from users.models import TestsFolder
+
+class TestsFolderModelTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+
+    def test_folder_creation(self):
+        folder = TestsFolder.objects.create(owner=self.user, folder_name='Test Folder')
+
+        self.assertEqual(folder.folder_name, 'Test Folder')
+        self.assertEqual(folder.owner, self.user)
+        self.assertTrue(isinstance(folder, TestsFolder))
+        self.assertEqual(str(folder), f"Folder: Test Folder by {self.user.username}")
--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include 
-from .views import  ProtectedView, AdminOnlyView, UserRegisterAPIView, ProfileViewSet, UserRegister, LogoutView, EditProfileView , profile , follow_user , unfollow_user, user_tests, create_folder, my_folders
+from .views import  ProtectedView, AdminOnlyView, UserRegisterAPIView, ProfileViewSet, UserRegister, LogoutView, EditProfileView, delete_folder, edit_folder, folder_detail , profile , follow_user , unfollow_user, user_tests, create_folder, my_folders
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -21,6 +21,10 @@ urlpatterns = [
     path('profile/<int:user_id>/follow/', follow_user, name='follow_user'),
     path('profile/<int:user_id>/unfollow/', unfollow_user, name='unfollow_user'),
     path('my-tests/', user_tests, name='user-tests'),
+    
     path('folders/create/', create_folder, name='create-folder'),
     path('folders/', my_folders, name='my-folders'),
+    path('folders/<int:folder_id>/', folder_detail, name='folder-detail'),
+    path('folders/<int:folder_id>/edit/', edit_folder, name='edit-folder'),
+    path('folders/<int:folder_id>/delete/', delete_folder, name='delete-folder'),
 ]
